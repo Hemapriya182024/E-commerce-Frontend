a1d585da3196539a38940c7b71fe0f79ef55ef1d
Hemapriya182024 <hemakarthikeyan2024@gmail.com>
2024-10-26 10:34:03 +0530

    err-corrected

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
@@ -6,11 +7,11 @@ import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './Context/ShopContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ShopContextProvider>
-      <App />
-
-    </ShopContextProvider>
-
-  </BrowserRouter>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
